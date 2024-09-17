4394e651b77c5806aa2e0e986334b903b937ce86
huylem1800vn <huylem1800vn@gmail.com>
2024-09-17 22:09:56 +0700

    Cài đặt CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const env = require("dotenv");
 env.config();
+var cors = require('cors');
 var bodyParser = require('body-parser');
 const database = require("./config/database");
 database.connect();
@@ -8,7 +9,14 @@ const app = express();
 const port = process.env.port;
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// app.use(cors({
+//     origin: 'http://xyz.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }));
 
 const routesApiV1 = require("./v1/routes/index.route");
 
