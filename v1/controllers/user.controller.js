a5ffc93327314549f65f55e56cf6ba8e75eefbbd
huylem1800vn <huylem1800vn@gmail.com>
2024-09-19 01:07:02 +0700

    Màn hình nhập Email lấy mã OTP

--- v1/controllers/user.controller.js
+++ v1/controllers/user.controller.js
@@ -1,7 +1,10 @@
 const md5 = require("md5");
 
 const User = require("../models/user.model");
-const generate = require("../../helpers/generate.helper");
+const ForgotPassword = require("../models/forgot-password.model");
+const generateHelper = require("../../helpers/generate.helper");
+const sendEmailHelper = require("../../helpers/sendEmail.helper");
+
 
 // [POST] /api/v1/users/register
 module.exports.register = async (req, res) => {
@@ -22,7 +25,7 @@ module.exports.register = async (req, res) => {
         fullName: req.body.fullName,
         email: req.body.email,
         password: md5(req.body.password),
-        token: generate.generateRandomString(30),
+        token: generateHelper.generateRandomString(30),
     };
 
     const user = new User(dataUser);
@@ -70,5 +73,59 @@ module.exports.login = async (req, res) => {
         code: 200,
         message: "Đăng nhập thành công!",
         token: token,
+    });
+};
+
+// [POST] /api/v1/users/password/forgot
+module.exports.passwordForgot = async (req, res) => {
+    const email = req.body.email;
+    const existUser = await User.findOne({
+        email: req.body.email,
+        deleted: false,
+    })
+
+    if(!existUser) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại!",
+        });
+        return;
+    }
+
+    // tạo chuỗi OTP gồm 6 số
+    const otp = generateHelper.generateRandomNumber(6);
+
+    // Việc 1: Lưu email vào database
+    const objectForgotPassword = {
+        email: req.body.email,
+        otp: otp,
+        // 3*60*1000 là 3 phút đc quy ra đơn vị
+        expireAt: Date.now() + 3*60*1000,
+    };
+    console.log(objectForgotPassword);
+    
+    const forgotPassword = new ForgotPassword(objectForgotPassword);
+    await forgotPassword.save();
+
+    // Việc 2: Gửi mã OTP qua mail cho người dùng
+    const subject = "Password Reset OTP Verification";
+    const text = `Dear ${existUser.fullName},
+
+    You recently requested to reset your password for your account. Please use the following One-Time Password (OTP) to verify your identity and complete the password reset process:
+    
+    OTP Code: ${otp}
+    
+    This OTP is valid for a limited time and should be used immediately. If you did not request a password reset, please ignore this email.
+    
+    Thank you for using our services.
+    
+    Best regards,
+    HMH Company`;
+    sendEmailHelper.sendEmail(email, subject, text);
+
+
+    res.json({
+        code: 200,
+        message: "Đã gửi mã OTP qua email",
     });
 };
